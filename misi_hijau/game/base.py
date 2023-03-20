e75b8d11331aa1707c56ee32b05a064722a24fcb
DaringCuteSeal <daringcuteseal@gmail.com>
2023-03-20 11:21:51 +0700

    switch game concept; rework player movement

--- misi_hijau/game/base.py
+++ misi_hijau/game/base.py
@@ -18,7 +18,7 @@
 import pyxel
 from typing import Callable
 from enum import Enum
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 # Enums
 class Direction(Enum):
@@ -31,22 +31,16 @@ class SoundType(Enum):
     AUDIO = 0
     MUSIC = 1
 
+class PlayerShip(Enum):
+    SHIP1 = 0
+    SHIP2 = 1
+    SHIP3 = 2
+
 # Other Constants
 ALPHA_COL = pyxel.COLOR_PURPLE
 WINDOW_WIDTH = 256
 WINDOW_HEIGHT = 256
-MAP_WIDTH = 24 * 8
-MAP_HEIGHT = 97 * 8
 TILE_SIZE = 8
-
-# Functions for Classes
-# XXX remove if not needed
-def tile_to_real(size: int):
-    """
-    Get real tile size from a tilemap scale.
-    """
-    return size * TILE_SIZE
-
 
 # Keyboard handling
 class KeyTypes(Enum):
@@ -95,6 +89,36 @@ class KeyListener:
                         if pyxel.btnp(check[k].binding, hold=check[k].hold_time, repeat=check[k].repeat_time):
                             check[k].func()
 
+# Level handling
+@dataclass
+class LevelMap:
+    """
+    A level map.
+    """
+    map_x: int # Offset x of tilemap
+    map_y: int # Offset y of tilemap
+    level_width: int
+    level_height: int
+
+@dataclass
+class Level:
+    """
+    A level.
+    """
+    idx: int
+    ship: PlayerShip
+    levelmap: LevelMap
+
+class LevelHandler:
+    def __init__(self, levels: list[Level]):
+        self.levels = levels
+        self.curr_level = levels[0]
+    
+    def set_lvl(self, level: Level):
+        self.curr_level = level
+
+    def get_curr(self) -> Level:
+        return self.curr_level
 
 # Camera handling
 @dataclass
@@ -102,35 +126,36 @@ class Camera:
     """
     A 2D camera.
     """
-    speed: int = 2
+    speed: int = 8
     x: int = 0
     y: int = 0
 
     def __init__(self):
        pyxel.camera()
    
-    def draw(self):
-        pyxel.bltm(0, 0, 0, self.x , self.y, 256, 256)
+    def draw(self, levelmap: LevelMap):
+        pyxel.bltm(0, 0, 0, self.x + levelmap.map_x, self.y + levelmap.map_y, 256, 256)
 
 
 # Text handling
 # TODO
 
 # Tick handling
+# XXX An entity SHOULD have their own ticker. If there's only one GLOBAL ticker, everything would be messed up.
 class Ticker():
     """
     Retro games aren't meant to be smooth. However, Pyxel supports high frame rate. This timer can be used to limit a rate of something without messing with the game's actual FPS.
     """
     def __init__(self):
         """
-        Initialize a new tick timer.
+        Initialize a new tick timer for an entity.
         """
         self.time_since_last_move = 0
         self.time_last_frame = 0
     
     def update(self):
         """
-        Update tick counts. Should be run on every game tick by the main game process. Shouldn't be called within sprites.
+        Update tick counts. Should be run on every game tick by sprite.
         """
         time_this_frame = pyxel.frame_count
         self.dt = time_this_frame - self.time_last_frame
@@ -193,9 +218,9 @@ class SpriteObj:
     speed: int = 1
     colkey: int | None = None
     costume_i: int = 0
-    keybindings: dict[str, KeyFunc] = {}
-    soundbank: dict[str, Sfx] = {}
-    costumes: dict[str, tuple[int, int]] = {}
+    keybindings: dict[str, KeyFunc] = field(default_factory=dict[str, KeyFunc])
+    soundbank: dict[str, Sfx] = field(default_factory=dict[str, Sfx])
+    costumes: dict[str, tuple[int, int]] = field(default_factory=dict[str, tuple[int, int]])
 
     def draw(self):
         """
@@ -229,19 +254,27 @@ class GameStateManager:
     camera: Camera
     ticker: Ticker
     keylistener: KeyListener
+    levelhandler: LevelHandler
 
 # Functions
-def map_to_view(sprite: SpriteObj, camera_pos: Coordinate) -> tuple[int, int] | bool:
+def map_to_view(sprite: SpriteObj, camera_pos: tuple[int, int]) -> tuple[int, int] | bool:
     """
     Get position of object in screen based on its position on the map. Returns `False` if object is not within the camera boundary.
     """
-    screen_x = sprite.coord.x_map - camera_pos.x
-    screen_y = sprite.coord.y_map - camera_pos.y
+
+    screen_x = sprite.coord.x_map - camera_pos[0]
+    screen_y = sprite.coord.y_map - camera_pos[1]
 
     if screen_x < 0 or screen_x > WINDOW_WIDTH or screen_y < 0 or screen_y > WINDOW_HEIGHT:
         return False
 
-    view_x = screen_x + (WINDOW_WIDTH // 2) - sprite.coord.x
-    view_y = screen_y + (WINDOW_HEIGHT // 2) - sprite.coord.y
+    return (screen_x, screen_y)
 
-    return view_x, view_y
+def tile_to_real(size: int) -> int:
+    """
+    Get real tile size from a tilemap scale.
+    """
+    return size * TILE_SIZE
+
+def round_to_tile(size: int) -> int:
+    return pyxel.ceil(size / TILE_SIZE) * TILE_SIZE
